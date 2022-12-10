eff714dd203ccd98983702f2087f40e434761493
AmelKrestic <92975105+AmelKrestic@users.noreply.github.com>
2022-12-10 13:25:04 +0100

    Update buildBenchmark.cmd

--- buildBenchmark.cmd
+++ buildBenchmark.cmd
@@ -1,6 +1,6 @@
 @echo off
-javac -d ".\build" .\src\standard\RoutePlanner.java
-javac -classpath ".\build" -d ".\build" .\src\benchmark\Benchmark.java
+javac -target 7 -d ".\build" .\src\standard\RoutePlanner.java
+javac -target 7 -classpath ".\build" -d ".\build" .\src\benchmark\Benchmark.java
 set /p args= "Input (all) arguments with proper Syntax: "
 @echo on
 java -classpath ".\build"  benchmark.Benchmark  %args%
