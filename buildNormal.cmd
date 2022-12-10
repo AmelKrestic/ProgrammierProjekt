ba398da07eb6696803e5a051d339f9f2870c5466
AmelKrestic <92975105+AmelKrestic@users.noreply.github.com>
2022-12-10 13:25:23 +0100

    Update buildNormal.cmd

--- buildNormal.cmd
+++ buildNormal.cmd
@@ -1,6 +1,6 @@
 @echo off
-javac -d ".\build" .\src\standard\RoutePlanner.java
-javac -classpath ".\build" -d ".\build" .\src\standard\Main.java
+javac -target 7 -d ".\build" .\src\standard\RoutePlanner.java
+javac -target 7 -classpath ".\build" -d ".\build" .\src\standard\Main.java
 set /p args= "Input graph path (without -graph): "
 @echo on
 java -classpath ".\build"  standard.Main  -graph %args%
