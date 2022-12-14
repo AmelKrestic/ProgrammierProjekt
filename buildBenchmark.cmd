b1cba6662503dff379ebd46fed932423ed7e1659
AmelKrestic <92975105+AmelKrestic@users.noreply.github.com>
2022-12-14 22:04:17 +0100

    Delete buildBenchmark.cmd

--- buildBenchmark.cmd
+++
@@ -1,8 +0,0 @@
-@echo off
-javac -source 7 -target 7 -d ".\build" .\src\standard\RoutePlanner.java
-javac -source 7 -target 7 -classpath ".\build" -d ".\build" .\src\benchmark\Benchmark.java
-set /p args= "Input (all) arguments with proper Syntax: "
-@echo on
-java -classpath ".\build"  benchmark.Benchmark  %args%
-
-@pause
