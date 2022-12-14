71fb4c72f938aa46d11809d55db2ef873511605c
AmelKrestic <92975105+AmelKrestic@users.noreply.github.com>
2022-12-14 22:04:11 +0100

    Delete buildNormal.cmd

--- buildNormal.cmd
+++
@@ -1,8 +0,0 @@
-@echo off
-javac -source 7 -target 7 -d ".\build" .\src\standard\RoutePlanner.java
-javac -source 7 -target 7 -classpath ".\build" -d ".\build" .\src\standard\Main.java
-set /p args= "Input graph path (without -graph): "
-@echo on
-java -classpath ".\build"  standard.Main  -graph %args%
-
-@pause
